9ae874966d344761028d8e027172e2939d044b5d
preciousnwaoha <nwaohaprecious25@gmail.com>
2022-06-12 00:04:59 +0100

    Lesson 4 done

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+    
+    // lodash
+    const num = _.random(0, 20);
+    console.log(num);
+
+    const greet = _.once(() => {
+        console.log("Hello");
+    });
+
+    greet();
+    greet();
+    greet();
 
     // set header content type
     res.setHeader('Content-Type', 'text/html');
@@ -18,7 +30,7 @@ const server = http.createServer((req, res) => {
             path += 'about.html';
             res.statusCode = 200;
             break;
-        case '/about-me':
+        case '/about-us':
             res.statusCode = 301;
             res.setHeader('Location', '/about');
             res.end();
